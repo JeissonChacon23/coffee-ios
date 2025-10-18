2074a886c5e89ce55de44a0b064d3f8d06e09835
Jeisson Chacón <jeissonchaconjp@gmail.com>
2025-10-18 14:10:57 -0500

    First commit

---
+++ coffee-ios/coffee_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffee_iosApp.swift
+//  coffee-ios
+//
+//  Created by Jeisson Chacon on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct coffee_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
